2e958b78eca9181e709dde4736a6cb2eedf4d831
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2023-11-30 10:41:12 +0530

    checking address

--- services/addresses/AddAddressService.js
+++ services/addresses/AddAddressService.js
@@ -2,13 +2,6 @@ const Address = require("../../models/address.mode");
 
 async function AddAddressService(userID, name, email, phone, country_id, state_id, city_id, address, type, default_shipping, default_billing) {
     try {
-        if (!country_id || !state_id || !city_id || !address || !type || !default_shipping || !default_billing) {
-            return res.status(400).json({
-                status: false,
-                message: 'Fill up all the fields!!'
-            })
-        }
-
         const newAddress = await Address.create({ user_id: userID, name, email, phone, country_id, state_id, city_id, address, type, default_shipping, default_billing })
         
         return {
