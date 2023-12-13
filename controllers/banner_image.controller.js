3b6b9974c49c798e6bbc308657cb9187d1f2c5fe
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2023-12-13 15:25:21 +0530

    changes in banner

--- controllers/banner_image.controller.js
+++ controllers/banner_image.controller.js
@@ -16,12 +16,15 @@ const upload = multer({
 
 async function postingBannerController(req, res) {
     try {
-        const image = 'uploads/banner-images' + req.file.filename;;
+        let image = '';
+        if (req.file){
+            image = 'uploads/banner-images/' + req.file.filename;;
+        }
 
         const { header, title, link, banner_location, after_product_qty, status, title_one, title_two, badge, product_slug } = req.body;
 
         const bannerInsert = await Banner_Image.create({ header, title, link, image, banner_location, after_product_qty, status, title_one, title_two, badge, product_slug });
-
+        console.log(bannerInsert);
         return res.status(500).json({
             status: true,
             message: "Banner Posted Successfully.",
