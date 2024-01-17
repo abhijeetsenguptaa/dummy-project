ba5cecc8f9a0b6531273bf4feaed3ba04d6fcb97
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2024-01-17 14:36:06 +0530

    banners deleted

--- routes/banner_image.routes.js
+++ routes/banner_image.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { postingBannerController, upload, fetchingBannerController } = require('../controllers/banner_image.controller');
+const { postingBannerController, upload, fetchingBannerController, deleteBannerController } = require('../controllers/banner_image.controller');
 const bannerImageRoute = express.Router();
 
 bannerImageRoute.post('/', upload.single('image'), postingBannerController);
 bannerImageRoute.get('/', fetchingBannerController);
+bannerImageRoute.delete('/:id', deleteBannerController);
+
 module.exports = bannerImageRoute;
